f8aaa78f7299f118217e09d8c1871439f427a139
imthiyasv91351 <imthiyasv81450@gmail.com>
2021-08-23 10:46:43 +0530

    First commit

---
+++ MINI PROJECT/client/index.js
@@ -0,0 +1,46 @@
+const selectEmployee = document.getElementById('employee-select');
+const ipStartDate = document.getElementById('ipStartDate');
+const ipEndDate = document.getElementById('ipEndDate');
+const btnTrack = document.getElementById('btnTrack');
+
+const resultCard = document.getElementById('resultCard');
+const vtName = document.getElementById('vtName');
+const vtHours = document.getElementById('vtHours');
+const btnCheck = document.getElementById('btnCheck');
+
+document.addEventListener('DOMContentLoaded', function () {
+	fetch('http://localhost:5000/api/get-employees')
+		.then((response) => response.json())
+		.then((data) => {
+			const { employees } = data;
+			let optionsMarkup = '<option value="">Select an employee</option>';
+			for (let i = 0; i < employees.length; i++) {
+				const employee = employees[i];
+				optionsMarkup += `<option value="${employee.eid}">${employee.eid} - ${employee.name}</option>`;
+			}
+			selectEmployee.innerHTML = optionsMarkup;
+		});
+});
+
+
+btnTrack.onclick = () => {
+	const start = Number(new Date(ipStartDate.value));
+	const end = Number(new Date(ipEndDate.value));
+    const eid = selectEmployee.value;
+
+    fetch(`http://localhost:5000/api/track-time?eid=${eid}&start=${start}&end=${end}`)
+		.then((response) => response.json())
+		.then((data) => {
+			// if (data.hours === undefined) {
+            //     swal("", data.message || 'something went wrong', "error");
+            //     return;
+            // }
+            vtName.innerText = data.employee.eid + ' - ' + data.employee.name;
+            vtHours.innerText = data.hours + ' Hours';
+            resultCard.style.display = 'block';
+		});
+};
+
+btnCheck.onclick = () => {
+    window.location.reload();
+}
---
+++ MINI PROJECT/server/index.js
@@ -0,0 +1,138 @@
+const express = require('express');
+const cors = require('cors');
+const dbService = require('./dbService');
+
+const app = express();
+
+app.use(cors());
+
+// const employees = [
+// 	{
+// 		eid: 1,
+// 		name: 'shaheem',
+// 	},
+// 	{
+// 		eid: 2,
+// 		name: 'imthyas',
+// 	},
+// 	{
+// 		eid: 3,
+// 		name: 'misha',
+// 	},
+// 	{
+// 		eid: 4,
+// 		name: 'jini',
+// 	},
+// ];
+
+// const worklog = [
+// 	{
+// 		eid: 1,
+// 		date: '2021-08-14',
+// 		time: 8,
+// 	},
+// 	{
+// 		eid: 1,
+// 		date: '2021-08-13',
+// 		time: 7,
+// 	},
+// 	{
+// 		eid: 2,
+// 		date: '2021-08-14',
+// 		time: 8,
+// 	},
+// 	{
+// 		eid: 2,
+// 		date: '2021-08-13',
+// 		time: 7,
+// 	},
+// 	{
+// 		eid: 3,
+// 		date: '2021-08-14',
+// 		time: 8,
+// 	},
+// 	{
+// 		eid: 3,
+// 		date: '2021-08-13',
+// 		time: 7,
+// 	},
+// 	{
+// 		eid: 4,
+// 		date: '2021-08-14',
+// 		time: 8,
+// 	},
+// 	{
+// 		eid: 4,
+// 		date: '2021-08-13',
+// 		time: 7,
+// 	},
+// ];
+
+const formatDate = (timestamp) => {
+	var d = new Date(timestamp),
+		month = '' + (d.getMonth() + 1),
+		day = '' + d.getDate(),
+		year = d.getFullYear();
+
+	if (month.length < 2) month = '0' + month;
+	if (day.length < 2) day = '0' + day;
+
+	return [year, month, day].join('-');
+};
+
+app.get('/api/get-employees', async (req, res) => {
+	const db = dbService.getDbServiceInstance();
+
+	let employees;
+	try {
+		employees = await db.getAllEmployees();
+	} catch (error) {
+		res.status(500).json({
+			error: error,
+		});
+	}
+
+	res.status(200).json({
+		employees,
+	});
+});
+
+app.get('/api/track-time', async (req, res) => {
+	const { eid, start, end } = req.query;
+
+	const db = dbService.getDbServiceInstance();
+
+	const formattedStart = formatDate(Number(start));
+	const formattedEnd = formatDate(Number(end));
+
+	let worklog;
+	try {
+		worklog = await db.getEmployeeWork(eid, formattedStart, formattedEnd);
+	} catch (error) {
+		res.status(500).json({
+			error: error,
+		});
+	}
+
+	const hoursJsonData = JSON.parse(JSON.stringify(worklog));
+
+	let employees;
+	try {
+		employees = await db.getEmployeeByEid(eid);
+	} catch (error) {
+		res.status(500).json({
+			error: error,
+		});
+	}
+
+    const employeeJsonData = JSON.parse(JSON.stringify(employees));
+
+	res.status(200).json({
+		hours: hoursJsonData[0].hours,
+        employee: employeeJsonData[0]
+	});
+});
+
+app.listen(5000, () => {
+	console.log('server started running on port 5000');
+});
