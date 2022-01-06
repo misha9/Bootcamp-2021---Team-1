39d78c8122a57f48fa6633959c26e5cf41815c34
Rashadreigns123 <rashadreigns@gmail.com>
2022-01-06 17:44:33 +0530

    add expiry time to token

--- Notes app/notes-app/client/src/Router.js
+++ Notes app/notes-app/client/src/Router.js
@@ -1,110 +1,109 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, BrowserRouter, Route, Navigate } from 'react-router-dom';
-import { APP } from './constants/index';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+import { Routes, BrowserRouter, Route, Navigate } from "react-router-dom";
+import { APP } from "./constants/index";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const MainContainer = React.lazy(() =>
-	import('./components/main/containers/MainContainer')
+  import("./components/main/containers/MainContainer")
 );
 
 const LoginContainer = React.lazy(() =>
-	import('./components/login/containers/LoginContainer')
+  import("./components/login/containers/LoginContainer")
 );
 
 const Router = () => {
-	const [isLogged, setIsLogged] = useState(false);
-	const [loading, setLoading] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-	function handleResponse(response) {
-		//console.log(response);
-		return response.text().then((text) => {
-			const data = text && JSON.parse(text);
-			// console.log(data);
-			return data;
-		});
-	}
+  function handleResponse(response) {
+    //console.log(response);
+    return response.text().then((text) => {
+      const data = text && JSON.parse(text);
+      // console.log(data);
+      return data;
+    });
+  }
 
-	const setAuth = (b) => {
-		setIsLogged(b);
-	};
-	async function isAuth() {
-		try {
-			setLoading(true);
-			const AT = localStorage.getItem('token');
-			//console.log(localStorage.token);
+  const setAuth = (b) => {
+    setIsLogged(b);
+  };
+  async function isAuth() {
+    try {
+      setLoading(true);
+      const AT = localStorage.getItem("token");
+      //console.log(localStorage.token);
 
-			const response = await fetch(
-				'http://localhost:5000/api/is-verify',
-				{
-					method: 'GET',
-					headers: {
-						'Content-type': 'application/json',
-						Authorization: `Bearer ${AT}`,
-					},
-				}
-			);
-			const parseRes = await handleResponse(response);
-			//console.log(parseRes, "hello");
+      const response = await fetch("http://localhost:5000/api/is-verify", {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${AT}`,
+        },
+      });
+      const parseRes = await handleResponse(response);
+      //console.log(parseRes, "hello");
 
-			//console.log(parseRes);
+      //console.log(parseRes);
 
-			parseRes === true ? setAuth(true) : setAuth(false);
-		} catch (err) {
-			//console.log(err.message);
-		} finally {
-			setLoading(false);
-		}
-	}
+      parseRes === true ? setAuth(true) : setAuth(false);
+    } catch (err) {
+      //console.log(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
 
-	useEffect(() => {
-		isAuth();
-	}, [isLogged]);
+  useEffect(() => {
+    isAuth();
+  }, [isLogged]);
 
-	//console.log(loading);
+  //console.log(loading);
 
-	if (loading) {
-		return (
-			<div className='wrapper'>
-				<div className='loader'></div>
-			</div>
-		);
-	}
+  if (loading) {
+    return (
+      <div className="wrapper">
+        <div className="loader"></div>
+      </div>
+    );
+  }
 
-	return (
-		<BrowserRouter>
-			<Routes>
-				{/* {!isLogged ? (
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* {!isLogged ? (
           <Route path='/' element={<LoginContainer setAuth={setAuth} />} />
         ) : (
           <Route path='/main' element={<MainContainer setAuth={setAuth} />} />
         )} */}
-				<Route
-					path='/'
-					element={
-						!isLogged ? (
-							<LoginContainer setAuth={setAuth} />
-						) : (
-							<Navigate replace to='/main' />
-						)
-					}
-				/>
-				<Route
-					path='/main'
-					element={
-						isLogged ? (
-							<MainContainer setAuth={setAuth} />
-						) : (
-							<Navigate replace to='/' />
-						)
-					}
-				/>
-				<Route
-					path={APP.ROUTES.LANDING}
-					element={<MainContainer setAuth={setAuth} />}
-				/>
-			</Routes>
-		</BrowserRouter>
-	);
+        <Route
+          path="/"
+          exact
+          element={
+            !isLogged ? (
+              <LoginContainer setAuth={setAuth} />
+            ) : (
+              <Navigate replace to="/main" />
+            )
+          }
+        />
+        <Route
+          path="/main"
+          exact
+          element={
+            isLogged ? (
+              <MainContainer setAuth={setAuth} />
+            ) : (
+              <Navigate replace to="/" />
+            )
+          }
+        />
+        <Route
+          path={APP.ROUTES.LANDING}
+          element={<MainContainer setAuth={setAuth} />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
